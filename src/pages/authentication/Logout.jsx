97393d211486d0a999edac912ba823291711d35d
Rohit Prasad <pdrohit674@gmail.com>
2022-03-18 20:16:50 +0530

    feat: added P0 authentication pages - Login, Logout, Register

---
+++ src/pages/authentication/Logout.jsx
@@ -0,0 +1,81 @@
+import './Authentication.css'
+
+export function Logout() {
+  return (
+    <div className="home__container">
+      <nav className="navbar simple">
+        <a href="../index.html">
+          <h3 className="navbar__header">Games-Cart</h3>
+        </a>
+        <ul className="navbar__list">
+          <li className="navbar__list__items">
+            <input type="text" className="search" placeholder="  🔍Search" />
+          </li>
+          <li className="navbar__list__items">
+            <a href="./login.html">
+              <button className="btn btn__primary">Login</button>
+            </a>
+          </li>
+
+          <li className="navbar__list__items">
+            <span className="badge__icons">
+              <a href="./wishlist.html">
+                <i className="fa-regular fa-heart icon"></i>
+                <span className="badge__number badge__primary"> 5 </span>
+              </a>
+            </span>
+          </li>
+          <li className="navbar__list__items">
+            <span className="badge__icons">
+              <a href="./cart.html">
+                <i className="fa-solid fa-cart-shopping icon"></i>
+                <span className="badge__number badge__primary">5</span>
+              </a>
+            </span>
+          </li>
+        </ul>
+      </nav>
+
+      <div className="main__container">
+        <h5 className="text__center logged__out">
+          You have been successfully Logged Out.
+        </h5>
+        <div className="form__group">
+          <h4>Login</h4>
+          <label for="username">Email</label>
+          <input
+            type="text"
+            id="username"
+            name="username"
+            placeholder="Enter your email"
+            className="form__group__input"
+          />
+          <label for="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            placeholder="Enter your password"
+            className="form__group__input"
+          />
+          <div className="form__bottom">
+            <label className="text__md">
+              <input type="checkbox" />
+              Remember me
+            </label>
+            <span className="forgot__password text__bold text__md">
+              Forgot your password?
+            </span>
+          </div>
+          <button className="btn btn__primary login">
+            <i className="fas fa-sign-in-alt login__icon"></i>
+            Login
+          </button>
+          <div className="register text__center">
+            <a href="./register.html">Create a new account ></a>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
---
+++ src/pages/authentication/Register.jsx
@@ -0,0 +1,108 @@
+import './Authentication.css'
+export function Register() {
+  return (
+    <div className="home__container">
+      <nav className="navbar simple">
+        <a href="../index.html">
+          <h3 className="navbar__header">Games-Cart</h3>
+        </a>
+        <ul className="navbar__list">
+          <li className="navbar__list__items">
+            <input type="text" className="search" placeholder="  🔍Search" />
+          </li>
+          <li className="navbar__list__items">
+            <a href="./login.html">
+              <button className="btn btn__primary">Login</button>
+            </a>
+          </li>
+
+          <li className="navbar__list__items">
+            <a href="./wishlist.html">
+              <span className="badge__icons">
+                <i className="fa-regular fa-heart icon"></i>
+                <span className="badge__number badge__primary">5</span>
+              </span>
+            </a>
+          </li>
+          <li className="navbar__list__items">
+            <a href="./cart.html">
+              <span className="badge__icons">
+                <i className="fa-solid fa-cart-shopping icon"></i>
+                <span className="badge__number badge__primary">5</span>
+              </span>
+            </a>
+          </li>
+        </ul>
+      </nav>
+
+      <div className="main__container">
+        <div className="form__group">
+          <h4>Sign up</h4>
+          <div className="form__name">
+            <label for="first__name">
+              First Name
+              <input
+                type="text"
+                id="first__name"
+                name="first__name"
+                className="form__group__input"
+              />
+            </label>
+            <label for="last__name">
+              Last Name
+              <input
+                type="text"
+                id="last__name"
+                name="last__name"
+                className="form__group__input"
+              />
+            </label>
+          </div>
+          <label for="username">
+            Email
+            <input
+              type="text"
+              id="username"
+              name="username"
+              className="form__group__input"
+            />
+          </label>
+          <label for="pass">
+            Password
+            <input
+              type="password"
+              id="pass"
+              name="pass"
+              className="form__group__input"
+            />
+          </label>
+
+          <label for="confirm__password">
+            Confirm Password
+            <input
+              type="password"
+              id="confirm__password"
+              name="confirm__password"
+              className="form__group__input"
+            />
+          </label>
+          <label>
+            <input type="checkbox" className="show__password text__md" /> Show
+            Password
+          </label>
+          <div className="form__bottom">
+            <label className="text__md">
+              <input type="checkbox" />I accept all terms & conditions
+            </label>
+          </div>
+          <button className="btn btn__primary login">
+            Create a new Account
+          </button>
+          <div className="register text__center">
+            <a href="./login.html">Already have an account ></a>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
