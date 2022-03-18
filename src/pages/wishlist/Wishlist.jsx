5bbca66fb29b5bd753fab4982dfedafa655f0419
Rohit Prasad <pdrohit674@gmail.com>
2022-03-18 20:19:49 +0530

    Refactor: replaced class with className in Wishlist

--- src/pages/wishlist/Wishlist.jsx
+++ src/pages/wishlist/Wishlist.jsx
@@ -2,117 +2,117 @@ import { image } from '../../assets/images/index'
 import './Wishlist.css'
 export function Wishlist() {
   return (
-    <div class="home__container">
-      <nav class="navbar simple">
+    <div className="home__container">
+      <nav className="navbar simple">
         <a href="../index.html">
-          <h3 class="navbar__header">Games-Cart</h3>
+          <h3 className="navbar__header">Games-Cart</h3>
         </a>
-        <ul class="navbar__list">
-          <li class="navbar__list__items">
-            <input type="text" class="search" placeholder="  🔍Search" />
+        <ul className="navbar__list">
+          <li className="navbar__list__items">
+            <input type="text" className="search" placeholder="  🔍Search" />
           </li>
-          <li class="navbar__list__items">
+          <li className="navbar__list__items">
             <a href="./logout.html">
-              <button class="btn btn__primary">Logout</button>
+              <button className="btn btn__primary">Logout</button>
             </a>
           </li>
 
-          <li class="navbar__list__items">
-            <span class="badge__icons">
+          <li className="navbar__list__items">
+            <span className="badge__icons">
               <a href="./wishlist.html">
-                <i class="fa-regular fa-heart icon"></i>
-                <span class="badge__number badge__primary"> 5 </span>
+                <i className="fa-regular fa-heart icon"></i>
+                <span className="badge__number badge__primary"> 5 </span>
               </a>
             </span>
           </li>
-          <li class="navbar__list__items">
-            <span class="badge__icons">
+          <li className="navbar__list__items">
+            <span className="badge__icons">
               <a href="./cart.html">
-                <i class="fa-solid fa-cart-shopping icon"></i>
-                <span class="badge__number badge__primary">5</span>
+                <i className="fa-solid fa-cart-shopping icon"></i>
+                <span className="badge__number badge__primary">5</span>
               </a>
             </span>
           </li>
         </ul>
       </nav>
 
-      <div class="main__container">
-        <h3 class="text__center font__regular">My Wishlist</h3>
+      <div className="main__container">
+        <h3 className="text__center font__regular">My Wishlist</h3>
 
-        <div class="wishlist">
-          <div class="card card__shadow">
-            <span class="card__icon right">
-              <i class="fas fa-heart heart"></i>
+        <div className="wishlist">
+          <div className="card card__shadow">
+            <span className="card__icon right">
+              <i className="fas fa-heart heart"></i>
             </span>
-            <span class="card__image">
+            <span className="card__image">
               <img src={image} alt="card Image" />
             </span>
-            <div class="card__footer">
-              <span class="card__title">Super Mario</span>
-              <span class="card__details">
-                <span class="card__details__price__new">₹2999</span>
-                <span class="card__details__price__old">₹3999</span>
-                <span class="card__details__discount">25% off</span>
+            <div className="card__footer">
+              <span className="card__title">Super Mario</span>
+              <span className="card__details">
+                <span className="card__details__price__new">₹2999</span>
+                <span className="card__details__price__old">₹3999</span>
+                <span className="card__details__discount">25% off</span>
               </span>
-              <div class="card__buttons">
-                <button class="btn btn__primary">Move to Cart</button>
+              <div className="card__buttons">
+                <button className="btn btn__primary">Move to Cart</button>
               </div>
             </div>
           </div>
-          <div class="card card__shadow">
-            <span class="card__icon right">
-              <i class="fas fa-heart heart"></i>
+          <div className="card card__shadow">
+            <span className="card__icon right">
+              <i className="fas fa-heart heart"></i>
             </span>
-            <span class="card__image">
+            <span className="card__image">
               <img src={image} alt="card Image" />
             </span>
-            <div class="card__footer">
-              <span class="card__title">Super Mario</span>
-              <span class="card__details">
-                <span class="card__details__price__new">₹2999</span>
-                <span class="card__details__price__old">₹3999</span>
-                <span class="card__details__discount">25% off</span>
+            <div className="card__footer">
+              <span className="card__title">Super Mario</span>
+              <span className="card__details">
+                <span className="card__details__price__new">₹2999</span>
+                <span className="card__details__price__old">₹3999</span>
+                <span className="card__details__discount">25% off</span>
               </span>
-              <div class="card__buttons">
-                <button class="btn btn__primary">Move to Cart</button>
+              <div className="card__buttons">
+                <button className="btn btn__primary">Move to Cart</button>
               </div>
             </div>
           </div>
-          <div class="card card__shadow">
-            <span class="card__icon right">
-              <i class="fas fa-heart heart"></i>
+          <div className="card card__shadow">
+            <span className="card__icon right">
+              <i className="fas fa-heart heart"></i>
             </span>
-            <span class="card__image">
+            <span className="card__image">
               <img src={image} alt="card Image" />
             </span>
-            <div class="card__footer">
-              <span class="card__title">Super Mario</span>
-              <span class="card__details">
-                <span class="card__details__price__new">₹2999</span>
-                <span class="card__details__price__old">₹3999</span>
-                <span class="card__details__discount">25% off</span>
+            <div className="card__footer">
+              <span className="card__title">Super Mario</span>
+              <span className="card__details">
+                <span className="card__details__price__new">₹2999</span>
+                <span className="card__details__price__old">₹3999</span>
+                <span className="card__details__discount">25% off</span>
               </span>
-              <div class="card__buttons">
-                <button class="btn btn__primary">Move to Cart</button>
+              <div className="card__buttons">
+                <button className="btn btn__primary">Move to Cart</button>
               </div>
             </div>
           </div>
-          <div class="card card__shadow">
-            <span class="card__icon right">
-              <i class="fas fa-heart heart"></i>
+          <div className="card card__shadow">
+            <span className="card__icon right">
+              <i className="fas fa-heart heart"></i>
             </span>
-            <span class="card__image">
+            <span className="card__image">
               <img src={image} alt="card Image" />
             </span>
-            <div class="card__footer">
-              <span class="card__title">Super Mario</span>
-              <span class="card__details">
-                <span class="card__details__price__new">₹2999</span>
-                <span class="card__details__price__old">₹3999</span>
-                <span class="card__details__discount">25% off</span>
+            <div className="card__footer">
+              <span className="card__title">Super Mario</span>
+              <span className="card__details">
+                <span className="card__details__price__new">₹2999</span>
+                <span className="card__details__price__old">₹3999</span>
+                <span className="card__details__discount">25% off</span>
               </span>
-              <div class="card__buttons">
-                <button class="btn btn__primary">Move to Cart</button>
+              <div className="card__buttons">
+                <button className="btn btn__primary">Move to Cart</button>
               </div>
             </div>
           </div>
