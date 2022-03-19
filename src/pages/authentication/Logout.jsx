128114eab460ec9b0eb7b098bb1261f03226b111
Rohit Prasad <pdrohit674@gmail.com>
2022-03-19 23:59:23 +0530

    feat: P0 for login and logout with css modules

--- src/pages/authentication/Logout.jsx
+++ src/pages/authentication/Logout.jsx
@@ -1,11 +1,11 @@
-import './Authentication.css'
+import auth from './Authentication.module.css'
 import { Navbar } from '../../components/navbar/Navbar.jsx'
 export function Logout() {
   return (
     <div className="home__container">
       <Navbar />
 
-      <div className="main__container">
+      {/* <div className="main__container">
         <h5 className="text__center logged__out">
           You have been successfully Logged Out.
         </h5>
@@ -42,6 +42,46 @@ export function Logout() {
           </button>
           <div className="register text__center">
             <a href="./register.html">Create a new account </a>
+          </div>
+        </div>
+      </div> */}
+      <div className={`${auth['main__container']}`}>
+        <h5 className={`text__center ${auth['logged__out']}`}>
+          You have been successfully Logged Out.
+        </h5>
+        <div className={`form__group ${auth['form__group']}`}>
+          <h4 className={auth['form__heading']}>Login</h4>
+          <label for="username">Email</label>
+          <input
+            type="text"
+            id="username"
+            name="username"
+            placeholder="Enter your email"
+            className={`form__group__input ${auth['input']}`}
+          />
+          <label for="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            placeholder="Enter your password"
+            className={`form__group__input ${auth['input']}`}
+          />
+          <div className={auth['form__bottom']}>
+            <label className="text__md">
+              <input type="checkbox" />
+              Remember me
+            </label>
+            <span className={`${auth['forgot__password']} text__bold text__md`}>
+              Forgot your password?
+            </span>
+          </div>
+          <button className={`btn btn__primary login ${auth['btn']}`}>
+            <i className="fas fa-sign-in-alt login__icon"></i>
+            Login
+          </button>
+          <div className={`${auth['register']} text__center`}>
+            <a href="./register.html">Create a new account {'>'} </a>
           </div>
         </div>
       </div>
