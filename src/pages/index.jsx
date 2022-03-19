cd4b9cf3f34010893305a068865008539f3a4e2e
Rohit Prasad <pdrohit674@gmail.com>
2022-03-20 01:46:00 +0530

    refactor: importing all pages to one file and exporting

---
+++ src/pages/index.jsx
@@ -0,0 +1,9 @@
+import { Homepage } from './homepage/Homepage.jsx'
+import { Products } from './products/Products.jsx'
+import { Wishlist } from './wishlist/Wishlist.jsx'
+import { Cart } from './cart/Cart.jsx'
+import { Register } from './authentication/Register.jsx'
+import { Login } from './authentication/Login.jsx'
+import { Logout } from './authentication/Logout.jsx'
+
+export { Homepage, Products, Wishlist, Cart, Register, Login, Logout }
