9c81024fb067a1e2f2230a81c6f0a25e3ed2ae1a
Rohit Prasad <pdrohit674@gmail.com>
2022-03-24 00:30:28 +0530

    refactor: improved Product Reducer Function
    changed all If conditions in Product Reducer to switch case

--- src/reducer/productReducer.jsx
+++ src/reducer/productReducer.jsx
@@ -1,33 +1,4 @@
 function productReducer(state, action) {
-  if (action.type === 'Action')
-    return {
-      ...state,
-      category: { ...state.category, action: !state.category.action },
-    }
-  if (action.type === 'Adventure')
-    return {
-      ...state,
-      category: { ...state.category, adventure: !state.category.adventure },
-    }
-  if (action.type === 'Strategy')
-    return {
-      ...state,
-      category: { ...state.category, strategy: !state.category.strategy },
-    }
-  if (action.type === 'Arcade')
-    return {
-      ...state,
-      category: { ...state.category, arcade: !state.category.arcade },
-    }
-  if (action.type === 'Sports')
-    return {
-      ...state,
-      category: { ...state.category, sports: !state.category.sports },
-    }
-  if (action.type === 'PRICE_RANGE') {
-    return { ...state, rangeLimit: parseInt(action.payload, 10) }
-  }
-
   switch (action.type) {
     case 'LOW_TO_HIGH':
       return { ...state, sortBy: 'LOW_TO_HIGH' }
@@ -41,6 +12,33 @@ function productReducer(state, action) {
       return { ...state, rating: '2_STARS_ABOVE' }
     case '1_STARS_ABOVE':
       return { ...state, rating: '1_STARS_ABOVE' }
+    case 'PRICE_RANGE':
+      return { ...state, rangeLimit: parseInt(action.payload, 10) }
+    case 'Sports':
+      return {
+        ...state,
+        category: { ...state.category, sports: !state.category.sports },
+      }
+    case 'Arcade':
+      return {
+        ...state,
+        category: { ...state.category, arcade: !state.category.arcade },
+      }
+    case 'Strategy':
+      return {
+        ...state,
+        category: { ...state.category, strategy: !state.category.strategy },
+      }
+    case 'Action':
+      return {
+        ...state,
+        category: { ...state.category, action: !state.category.action },
+      }
+    case 'Adventure':
+      return {
+        ...state,
+        category: { ...state.category, adventure: !state.category.adventure },
+      }
     default:
       return { ...state }
   }
