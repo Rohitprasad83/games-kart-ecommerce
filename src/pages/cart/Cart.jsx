e0679d4951d1cb5611e82f4c242a9d069df295b3
Rohit Prasad <pdrohit674@gmail.com>
2022-03-27 18:13:34 +0530

    style: improved styling for cart page

--- src/pages/cart/Cart.jsx
+++ src/pages/cart/Cart.jsx
@@ -14,52 +14,70 @@ export function Cart() {
       <Navbar />
 
       <div className={`main__container ${cartStyle['main__container']}`}>
-        {cartItems.products.map(product => (
-          <CartProductCard key={product._id} cartProduct={product} />
-        ))}
-        {cartItems.products.length === 0 ? (
-          <div className={cartStyle['flex__column']}>
-            <div className="text__lg text__center">Your Cart is Empty</div>
-            <div className="text__lg text__center">
-              Add Products to Your Cart
+        <div className={cartStyle['cart__summary']}>
+          {cartItems.products.length === 0 ? (
+            <div className={cartStyle['flex__column']}>
+              <div className="text__lg text__center">Your Cart is Empty</div>
+              <div className="text__lg text__center">
+                Add Products to Your Cart
+              </div>
+              <Link to="/products" className="text__center">
+                <button className="btn btn__primary text__lg">Shop Now</button>
+              </Link>
             </div>
-            <Link to="/products" className="text__center">
-              <button className="btn btn__primary text__lg">Shop Now</button>
-            </Link>
-          </div>
-        ) : (
-          <div className={cartStyle['cart']}>
-            <h5>Price Details</h5>
-            <hr />
-            <div className={`${cartStyle['cart__details']} text__md`}>
-              Price({cartItems.products.length})
-              <span className="text__right">₹{totalPrice}</span>
+          ) : (
+            <div className={cartStyle['cart']}>
+              <h5>Price Details</h5>
+              <hr />
+              <div className={`${cartStyle['flex__column']} text__md`}>
+                <div className={cartStyle['flex__row']}>
+                  <span> Name</span>
+                  <span> Quantity</span>
+                </div>
+                {cartItems.products.map(product => {
+                  return (
+                    <div className={cartStyle['flex__row']}>
+                      <span>{product.title}</span>
+                      <span className="text__right"> {product.quantity}</span>
+                    </div>
+                  )
+                })}
+              </div>
+              <div className={`${cartStyle['cart__details']} text__md`}>
+                Total Price
+                <span className="text__right">₹{totalPrice}</span>
+              </div>
+              <div className={`${cartStyle['cart__details']} text__md`}>
+                Discount
+                <span>-₹{totalDiscount}</span>
+              </div>
+              <div className={`${cartStyle['cart__details']} text__md`}>
+                Delivery Charges
+                <span>₹499</span>
+              </div>
+              <hr />
+              <h5 className={cartStyle['cart__details']}>
+                Total Amount
+                <span className="text__lg">
+                  ₹{totalPrice - totalDiscount - 499}
+                </span>
+              </h5>
+              <hr />
+              <div className="text__md">
+                You will save ₹{totalDiscount} on this order
+              </div>
+              <button
+                className={`btn btn__primary font__bold ${cartStyle['btn']}`}>
+                Check out
+              </button>
             </div>
-            <div className={`${cartStyle['cart__details']} text__md`}>
-              Discount
-              <span>-₹{totalDiscount}</span>
-            </div>
-            <div className={`${cartStyle['cart__details']} text__md`}>
-              Delivery Charges
-              <span>₹499</span>
-            </div>
-            <hr />
-            <h5 className={cartStyle['cart__details']}>
-              Total Amount
-              <span className="text__lg">
-                ₹{totalPrice - totalDiscount - 499}
-              </span>
-            </h5>
-            <hr />
-            <div className="text__md">
-              You will save ₹{totalDiscount} on this order
-            </div>
-            <button
-              className={`btn btn__primary font__bold ${cartStyle['btn']}`}>
-              Check out
-            </button>
-          </div>
-        )}
+          )}
+        </div>
+        <div className={cartStyle['cart__container']}>
+          {cartItems.products.map(product => (
+            <CartProductCard key={product._id} cartProduct={product} />
+          ))}
+        </div>
       </div>
     </div>
   )
