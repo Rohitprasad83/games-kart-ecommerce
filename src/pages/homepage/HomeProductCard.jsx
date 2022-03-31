cdd810b090014dd97bc2d6a85c0a8ec5a64f67dd
Rohit Prasad <pdrohit674@gmail.com>
2022-04-01 00:43:44 +0530

    feat: filter applied from homepage

--- src/pages/homepage/HomeProductCard.jsx
+++ src/pages/homepage/HomeProductCard.jsx
@@ -1,11 +1,15 @@
 import { Link } from 'react-router-dom'
+import { useProductFilter } from '../../context/index.jsx'
 const HomeProductCard = ({ category }) => {
+  const { productDispatch } = useProductFilter()
   const { categoryName, img } = category
   return (
-    <div className="card card__shadow">
+    <div
+      className="card card__shadow"
+      onClick={() => productDispatch({ type: categoryName })}>
       <Link to="/products">
         <span className="card__text__overlay"> {categoryName}</span>
-        <img src={img} alt="Action" className="responsive__img" />
+        <img src={img} alt={categoryName} className="responsive__img" />
       </Link>
     </div>
   )
