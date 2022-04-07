bf8cc785b10730569285197d83011fb689c08276
Rohit Prasad <pdrohit674@gmail.com>
2022-04-07 21:35:15 +0530

    feat: added Logout

--- src/App.js
+++ src/App.js
@@ -2,58 +2,36 @@ import './App.css'
 import { Routes, Route } from 'react-router-dom'
 import Mockman from 'mockman-js'
 import {
-    Homepage,
-    Products,
-    Wishlist,
-    Cart,
-    Register,
-    Login,
-    Logout,
-    Profile,
+  Homepage,
+  Products,
+  Wishlist,
+  Cart,
+  Register,
+  Login,
+  Logout,
+  Profile,
 } from './pages/index'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 
 function App() {
-    return ( <
-        div className = "App" >
-        <
-        Routes >
-        <
-        Route path = "/"
-        element = { < Homepage / > }
-        /> <
-        Route path = "/products"
-        element = { < Products / > }
-        /> <
-        Route path = "/cart"
-        element = { < Cart / > }
-        /> <
-        Route path = "/wishlist"
-        element = { < Wishlist / > }
-        /> <
-        Route path = "/login"
-        element = { < Login / > }
-        /> <
-        Route path = "/logout"
-        element = { < Logout / > }
-        /> <
-        Route path = "/register"
-        element = { < Register / > }
-        /> <
-        Route path = "/profile"
-        element = { < Profile / > }
-        /> <
-        Route path = "/mock"
-        element = { < Mockman / > }
-        /> <
-        /Routes>
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/wishlist" element={<Wishlist />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/logout" element={<Logout />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/mock" element={<Mockman />} />
+      </Routes>
 
-        <
-        ToastContainer / >
-        <
-        /div>
-    )
+      <ToastContainer />
+    </div>
+  )
 }
 
 export default App
