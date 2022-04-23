b3cfa45bcf5ffb16beb3d091dbeadace96178883
Rohit Prasad <pdrohit674@gmail.com>
2022-04-24 01:26:05 +0530

    feat: single product page styling

--- src/pages/products/SingleProduct.jsx
+++ src/pages/products/SingleProduct.jsx
@@ -3,7 +3,8 @@ import { Navbar } from 'components/navbar/Navbar.jsx'
 import { Footer } from 'components/footer/Footer.jsx'
 import { useParams, useNavigate } from 'react-router-dom'
 import { products } from 'backend/db/products'
-import { ProductCard } from './ProductCard'
+import productStyle from './Products.module.css'
+
 function SingleProduct() {
   let { productId } = useParams()
   const navigate = useNavigate()
@@ -16,7 +17,51 @@ function SingleProduct() {
     <div className="home__container">
       <Navbar />
       <div className="main__container">
-        <ProductCard product={product} />
+        <div className={`text__lg ${productStyle['single-product']}`}>
+          <div className={productStyle['product-image']}>
+            <img src={product.img} alt={product.title} />
+          </div>
+          <div className={productStyle['product-details']}>
+            <h3>{product.title}</h3>
+            <span className={productStyle['product-description']}>
+              <span className="font__bold">Description </span>-{' '}
+              {product.description}
+            </span>
+            <hr />
+
+            <span>
+              <span className="font__bold">Category </span>-{' '}
+              {product.categoryName}
+            </span>
+
+            <span>
+              <span className="font__bold">Availabilty </span>- In Stock
+            </span>
+            <span>
+              <span className="font__bold">Rating </span>- {product.rating}
+            </span>
+
+            <span className={productStyle['price']}>
+              <span className="font__bold">Price - </span>
+              <span>{product.price}</span>
+              <span className={productStyle['striked']}>
+                {product.oldPrice}
+              </span>
+              <span>{product.discount} Off</span>
+            </span>
+            <hr />
+            <span className={productStyle['buttons']}>
+              <button
+                className={`btn btn__primary ${productStyle['single-product-btn']}`}>
+                Add To Cart
+              </button>
+              <button
+                className={`btn btn__secondary ${productStyle['single-product-btn']}`}>
+                Add To Wishlist
+              </button>
+            </span>
+          </div>
+        </div>
       </div>
       <Footer />
     </div>
