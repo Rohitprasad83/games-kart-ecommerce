84076d85e8ed3fedb7d2de463351ba07b2938f02
Rohit Prasad <pdrohit674@gmail.com>
2022-06-16 00:16:21 +0530

    feat: address context

---
+++ src/context/address-context.jsx
@@ -0,0 +1,42 @@
+import { createContext, useContext, useReducer } from 'react'
+import { addressReducer } from 'reducer/addressReducer'
+import { v4 as uuid } from 'uuid'
+
+const addressContext = createContext()
+const useAddress = () => useContext(addressContext)
+
+const initialAddress = {
+  addresses: [
+    {
+      _id: uuid(),
+      name: 'Rohit',
+      mobile: '1234567890',
+      address: '5/1 XYZ Road',
+      district: 'abc district',
+      state: 'West Bengal',
+      pincode: '700028',
+    },
+    {
+      _id: uuid(),
+      name: 'Prasad',
+      mobile: '0987654321',
+      address: '5/1 ABC Road',
+      district: 'xyz district',
+      state: 'West Bengal',
+      pincode: '700028',
+    },
+  ],
+}
+const AddressContextProvider = ({ children }) => {
+  const [addressData, addressDispatch] = useReducer(
+    addressReducer,
+    initialAddress
+  )
+  return (
+    <addressContext.Provider value={{ addressData, addressDispatch }}>
+      {children}
+    </addressContext.Provider>
+  )
+}
+
+export { AddressContextProvider, useAddress }
